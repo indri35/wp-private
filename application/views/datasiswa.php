499f27864d91d5aada52dc200de65f95b2dec68b
Hendrik <drikdoank@gmail.com>
2017-08-06 20:13:14 +0700

    data siswa

---
+++ application/views/datasiswa.php
@@ -0,0 +1,429 @@
+<!doctype html>
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><html lang="en" class="no-js"> <![endif]-->
+<html lang="en">
+
+<head>
+
+  <!-- Basic -->
+  <title>ESC | Contact</title>
+
+  <!-- Define Charset -->
+  <meta charset="utf-8">
+
+  <!-- Responsive Metatag -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <!-- Page Description and Author -->
+  <meta name="description" content="Margo - Responsive HTML5 Template">
+  <meta name="author" content="iThemesLab">
+
+
+  <!-- Bootstrap CSS  -->
+  <link rel="stylesheet" href="<?php echo base_url()?>assets/asset/css/bootstrap.min.css" type="text/css" media="screen">
+
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="<?php echo base_url()?>assets/css/font-awesome.min.css" type="text/css" media="screen">
+
+  <!-- Slicknav -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/slicknav.css" media="screen">
+
+  <!-- Margo CSS Styles  -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/style.css" media="screen">
+
+  <!-- Responsive CSS Styles  -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/responsive.css" media="screen">
+
+  <!-- Responsive CSS Styles  -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/bootstrap.min.css" media="screen">
+
+  <!-- Responsive CSS Styles  -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/dataTables.bootstrap.min.css" media="screen">
+  
+  <!-- Responsive CSS Styles  -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/buttons.dataTables.min.css" media="screen">
+
+
+  <!-- Color CSS Styles  -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/red.css" title="red" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/jade.css" title="jade" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/blue.css" title="blue" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/beige.css" title="beige" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/cyan.css" title="cyan" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/green.css" title="green" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/orange.css" title="orange" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/peach.css" title="peach" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/pink.css" title="pink" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/purple.css" title="purple" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/sky-blue.css" title="sky-blue" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/colors/yellow.css" title="yellow" media="screen" />
+
+
+  <!-- Margo JS  -->
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery-2.1.4.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.migrate.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/modernizrr.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.fitvids.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/owl.carousel.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/nivo-lightbox.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.isotope.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.appear.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/count-to.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.textillate.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.lettering.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.easypiechart.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.nicescroll.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.parallax.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/dataTables.bootstrap.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/dataTables.buttons.min.js"></script>
+  <script type="text/javascript">     
+  $(document).ready(function() {
+      $('.table').DataTable();
+  } );
+</script>
+
+  <script type="text/javascript"> src="<?php echo base_url()?>assets/http://maps.googleapis.com/maps/api/js?sensor=false"</script>
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery.slicknav.js"></script>
+
+
+
+  <!--[if IE 8]><script src="<?php echo base_url()?>assets/http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <!--[if lt IE 9]><script src="<?php echo base_url()?>assets/http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+</head>
+
+<body>
+
+  <!-- Container -->
+  <div id="container">
+
+    <!-- Start Header -->
+    <div class="hidden-header"></div>
+    <header class="clearfix">
+
+      <!-- Start Top Bar -->
+      <div class="top-bar">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8">
+              <!-- Start Contact Info -->
+              <ul class="contact-details">
+                <li><a href="<?php echo base_url()?>assets/#"><i class="fa fa-map-marker"></i>  Jl. Rancakendal Luhur No 49, Bandung</a>
+                </li>
+                <li><a href="<?php echo base_url()?>assets/#"><i class="fa fa-envelope-o"></i> educationsolutionscenter@gmail.com</a>
+                </li>
+                <li><a href="<?php echo base_url()?>assets/#"><i class="fa fa-phone"></i> +62 858 9412 8283</a>
+                </li>
+              </ul>
+              <!-- End Contact Info -->
+            </div>
+            <div class="col-md-4">
+              <!-- Start Social Links -->
+              <ul class="social-list">
+                <li>
+                  <a class="facebook itl-tooltip" data-placement="bottom" title="Facebook" href="https://web.facebook.com/educationsolutioncentre"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li>
+                  <a class="instgram itl-tooltip" data-placement="bottom" title="Instagram" href="https://www.instagram.com/educationsolutioncenter"><i class="fa fa-instagram"></i></a>
+                </li>
+              </ul>
+              <!-- End Social Links -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Top Bar -->
+
+      <!-- Start Header ( Logo & Naviagtion ) -->
+      <div class="navbar navbar-default navbar-top">
+        <div class="container">
+          <div class="navbar-header">
+            <!-- Stat Toggle Nav Link For Mobiles -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <i class="fa fa-bars"></i>
+            </button>
+            <!-- End Toggle Nav Link For Mobiles -->
+            <a class="navbar-brand" href="<?php echo base_url()?>assets/index.html"><img alt="" src="<?php echo base_url()?>assets/images/logo.png"></a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <!-- Stat Search -->
+            <div class="search-side">
+              <a class="show-search"><i class="fa fa-search"></i></a>
+              <div class="search-form">
+                <form autocomplete="off" role="search" method="get" class="searchform" action="#">
+                  <input type="text" value="" name="s" id="s" placeholder="Search the site...">
+                </form>
+              </div>
+            </div>
+            <!-- End Search -->
+            <!-- Start Navigation List -->
+            <ul class="nav navbar-nav navbar-right">
+              <li>
+                <a href="<?php echo base_url()?>Welcome/index">Home</a>
+              </li>
+              <li>
+                <a class="active" href="<?php echo base_url()?>Welcome/data">Data Siswa</a>
+              </li>
+              <li>
+                <a href="<?php echo base_url()?>Welcome/about">About</a>
+              </li>
+              <li>
+                <a href="#">Program</a>
+                <ul class="dropdown">
+                  <li><a href="<?php echo base_url()?>Welcome/program">Reguler</a></li>
+                  <li><a href="<?php echo base_url()?>Welcome/program2">Unggulan</a></li>
+                  <li><a href="<?php echo base_url()?>Welcome/program3">Intensif UN/SBMPTN</a></li>
+                  <li><a href="<?php echo base_url()?>Welcome/program4">Eksklusif (1 semester & 2 semester)</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="<?php echo base_url()?>Welcome/registration">Registration</a>
+              </li>
+              <li><a  href="<?php echo base_url()?>Welcome/contact">Contact</a></li>
+            </ul>
+            <!-- End Navigation List -->
+          </div>
+        </div>
+
+        <!-- Mobile Menu Start -->
+        <ul class="wpb-mobile-menu">
+          <li>
+                <a href="<?php echo base_url()?>Welcome/index">Home</a>
+              </li>
+              <li>
+                <a href="<?php echo base_url()?>Welcome/about">About</a>
+              </li>
+              <li>
+                <a href="<?php echo base_url()?>Welcome/program">Program</a>
+                <ul class="dropdown">
+                  <li><a href="<?php echo base_url()?>Welcome/program">Reguler</a></li>
+                  <li><a href="<?php echo base_url()?>Welcome/program2">Unggulan</a></li>
+                  <li><a href="<?php echo base_url()?>Welcome/program3">Intensif UN/SBMPTN</a></li>
+                  <li><a href="<?php echo base_url()?>Welcome/program4">Eksklusif (1 semester & 2 semester)</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="<?php echo base_url()?>Welcome/registration">Registration</a>
+              </li>
+              <li><a  href="<?php echo base_url()?>Welcome/contact">Contact</a></li>
+        </ul>
+        <!-- Mobile Menu End -->
+
+      </div>
+      <!-- End Header ( Logo & Naviagtion ) -->
+
+    </header>
+    <!-- End Header -->
+
+    
+    <!-- Start Content -->
+    <div id="content">
+      <div class="container">
+          <div class="row">
+              <div class="col-sm-12">
+                  <table class="table">
+                      <thead class="dashboard-table-header">
+                          <tr>
+                              <th>No</th>
+                              <th>Nama</th>
+                              <th>TTL</th>
+                              <th>Jenis Kelamin</th>
+                              <th>Agama</th>
+                              <th>Sekolah</th>
+                              <th>Kelas</th>
+                              <th>HP</th>
+                              <th>Email</th>
+                              <th>Alamat</th>
+                              <th>Jadwal Les</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                      <? foreach ($siswa as $row) : ?>
+                          <tr>
+                          <td><? echo $row->id_student; ?></td>
+                          <td><? echo $row->nama; ?></td>
+                          <td><? echo $row->ttl; ?></td>
+                          <td><? echo $row->jenis_kelamin; ?></td>
+                          <td><? echo $row->agama; ?></td>
+                          <td><? echo $row->asal_sekolah; ?></td>
+                          <td><? echo $row->kelas; ?></td>
+                          <td><? echo $row->no_tlp; ?></td>
+                          <td><? echo $row->email; ?></td>
+                          <td><? echo $row->alamat; ?></td>
+                          <td><? echo $row->jadwal_les; ?></td>
+                          </tr>
+                          <? endforeach; ?>                                            
+                      </tbody>
+                  </table>
+              </div>
+          </div>
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <!-- Classic Heading -->
+            <h4 class="classic-title"><span>Information</span></h4>
+
+            <!-- Divider -->
+            <div class="hr1" style="margin-bottom:10px;"></div>
+
+            <!-- Info - Icons List -->
+            <ul class="icons-list">
+              <li><i class="fa fa-globe">  </i> <strong>Address:</strong> Jl. Rancakendal Luhur No 49, Bandung</li>
+              <li><i class="fa fa-envelope-o"></i> <strong>Email:</strong> educationsolutionscenter@gmail.com</li>
+              <li><i class="fa fa-mobile"></i> <strong>Phone:</strong> +62 858 9412 8283</li>
+            </ul>
+            <div class="hr1" style="margin-bottom:200px;"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End content -->
+
+
+    <!-- Start Footer -->
+    <footer>
+      <div class="container">
+
+        <!-- Start Copyright -->
+        <div class="copyright-section">
+          <div class="row">
+            <div class="col-md-12">
+              <center><p>&copy; 2017 EDUCATION SOLUTION CENTER - ALL RIGHTS RESERVED</a> </p></center>
+            </div>
+          </div>
+        </div>
+        <!-- End Copyright -->
+
+      </div>
+    </footer>
+    <!-- End Footer -->
+
+  </div>
+  <!-- End Container -->
+
+  <!-- Go To Top Link -->
+  <a href="<?php echo base_url()?>assets/#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+  <!-- Style Switcher -->
+  <div class="switcher-box">
+    <a class="open-switcher show-switcher"><i class="fa fa-cog fa-2x"></i></a>
+    <h4>Style Switcher</h4>
+    <span>12 Predefined Color Skins</span>
+    <ul class="colors-list">
+      <li>
+        <a onClick="setActiveStyleSheet('blue'); return false;" title="Blue" class="blue"></a>
+      </li>
+      <li>
+        <a onClick="setActiveStyleSheet('sky-blue'); return false;" title="Sky Blue" class="sky-blue"></a>
+      </li>
+      <li>
+        <a onClick="setActiveStyleSheet('cyan'); return false;" title="Cyan" class="cyan"></a>
+      </li>
+      <li>
+        <a onClick="setActiveStyleSheet('jade'); return false;" title="Jade" class="jade"></a>
+      </li>
+      <li>
+        <a onClick="setActiveStyleSheet('green'); return false;" title="Green" class="green"></a>
+      </li>
+      <li>
+        <a onClick="setActiveStyleSheet('purple'); return false;" title="Purple" class="purple"></a>
+      </li>
+      <li>
+        <a onClick="setActiveStyleSheet('pink'); return false;" title="Pink" class="pink"></a>
+      </li>
+      <li>
+        <a onClick="setActiveStyleSheet('red'); return false;" title="Red" class="red"></a>
+      </li>
+      <li>
+        <a onClick="setActiveStyleSheet('orange'); return false;" title="Orange" class="orange"></a>
+      </li>
+      <li>
+        <a onClick="setActiveStyleSheet('yellow'); return false;" title="Yellow" class="yellow"></a>
+      </li>
+      <li>
+        <a onClick="setActiveStyleSheet('peach'); return false;" title="Peach" class="peach"></a>
+      </li>
+      <li>
+        <a onClick="setActiveStyleSheet('beige'); return false;" title="Biege" class="beige"></a>
+      </li>
+    </ul>
+    <span>Top Bar Color</span>
+    <select id="topbar-style" class="topbar-style">
+      <option value="1">Light (Default)</option>
+      <option value="2">Dark</option>
+      <option value="3">Color</option>
+    </select>
+    <span>Layout Style</span>
+    <select id="layout-style" class="layout-style">
+      <option value="1">Wide</option>
+      <option value="2">Boxed</option>
+    </select>
+    <span>Patterns for Boxed Version</span>
+    <ul class="bg-list">
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg1"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg2"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg3"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg4"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg5"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg6"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg7"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg8"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg9"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg10"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg11"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg12"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg13"></a>
+      </li>
+      <li>
+        <a href="<?php echo base_url()?>assets/#" class="bg14"></a>
+      </li>
+    </ul>
+  </div>
+
+  <script type="text/javascript" src="<?php echo base_url()?>assets/js/script.js"></script>
+
+  <script type="text/javascript">
+    //Contact Form
+
+    $('#submit').click(function() {
+
+      $.post("php/send.php", $(".contact-form").serialize(), function(response) {
+        $('#success').html(response);
+      });
+      return false;
+
+    });
+  </script>
+
+</body>
+
+</html>
